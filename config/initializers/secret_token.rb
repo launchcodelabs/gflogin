4c615d7ca8b8197fd537bc04df33b1a048720090
Launch Code Labs <god@launchcodelabs.com>
2012-03-22 19:11:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gflogin::Application.config.secret_token = 'b414663d21355ffa7613cb479868fd29e884d44b20a69b74135c6785512bd5b7a8d60f8861584920867fa49c7e11127c7e5e5f39e363c166a317dfa24545a8c0'
