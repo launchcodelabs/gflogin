5825b0303a595789ed9ec3c59651f0173c409227
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-03-23 14:28:14 -0700

    added form validation and made the errors look pretty

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,2 +1,16 @@
 class Login < ActiveRecord::Base
+  attr_accessible :firstName
+  attr_accessible :lastName
+  attr_accessible :email
+  attr_accessible :company
+  
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :firstName, :presence => true
+  validates :lastName, :presence => true
+  
+  validates :email, :presence => true,
+                    :length => { :minimum => 3, :maximum => 254 },  
+                    :format     => { :with => email_regex }         
 end
